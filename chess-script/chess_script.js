802a9b54a02b3c09c4e1cc01f0a8df1f9b24313d
athorat <athorat@tavisca.com>
2014-08-31 20:59:15 +0530

    cmmiting

--- chess-script/chess_script.js
+++ chess-script/chess_script.js
@@ -1,6 +1,7 @@
 // java script file 
 
 //var chess=new chess();
+var elementId;
 
 var rook_black="<img src='images/rook_black.png' width=80%>";
 var knight_black="<img src='images/knight_black.png' width=80%>";
@@ -10,50 +11,79 @@ var king_black="<img src='images/king_black.png' width=80%>";
 var pawn_black="<img src='images/pawn_black.png' width=80%>";
 var pawn_white="<img src='images/pawn_white.png' width=80%>";
 
-
-
-
-
-
  function Start_chess()
  {
-  //alert("hiii");
+ // alert("hiii");
  
- document.getElementById("A1").innerHTML=rook_black;
- document.getElementById("A2").innerHTML=knight_black;
- document.getElementById("A3").innerHTML=bishop_black;
- document.getElementById("A4").innerHTML=queen_black;
- document.getElementById("A5").innerHTML=king_black;
- document.getElementById("A6").innerHTML=bishop_black;
- document.getElementById("A7").innerHTML=knight_black;
- document.getElementById("A8").innerHTML=rook_black;
+ document.getElementById("01").innerHTML=rook_black;
+ document.getElementById("02").innerHTML=knight_black;
+ document.getElementById("03").innerHTML=bishop_black;
+ document.getElementById("04").innerHTML=queen_black;
+ document.getElementById("05").innerHTML=king_black;
+ document.getElementById("06").innerHTML=bishop_black;
+ document.getElementById("07").innerHTML=knight_black;
+ document.getElementById("08").innerHTML=rook_black;
  
- document.getElementById("B1").innerHTML=pawn_black;
- document.getElementById("B2").innerHTML=pawn_black;
- document.getElementById("B3").innerHTML=pawn_black;
- document.getElementById("B4").innerHTML=pawn_black;
- document.getElementById("B5").innerHTML=pawn_black;
- document.getElementById("B6").innerHTML=pawn_black;
- document.getElementById("B7").innerHTML=pawn_black;
- document.getElementById("B8").innerHTML=pawn_black;
+ document.getElementById("11").innerHTML=pawn_black;
+ document.getElementById("12").innerHTML=pawn_black;
+ document.getElementById("13").innerHTML=pawn_black;
+ document.getElementById("14").innerHTML=pawn_black;
+ document.getElementById("15").innerHTML=pawn_black;
+ document.getElementById("16").innerHTML=pawn_black;
+ document.getElementById("17").innerHTML=pawn_black;
+ document.getElementById("18").innerHTML=pawn_black;
  
- 
- 
-
  }
  
 function clear_chess()
  {
+ alert("hiii");
   //var pawn_img = document.getElementById('pawn_black');
-    pawn_black.style.visibility = 'hidden';
+  /*  pawn_black.style.visibility = 'hidden';
 
 	var rook_img = document.getElementById('rook_black');
-    rook_img.style.visibility = 'hidden';
-	
+    rook_img.style.visibility = 'hidden';*/
 
-	
-// alert("hiii");
+ }
+ 
+ function divClick(element)
+ {
+ var color=element.firstChild.id.charAt(0);
+ var type= element.firstChild.id.charAt(1);
+ alert(color);
+ alert(type);
+var divId=element.id;
+ alert(divId);
+ 
+ elementId=element.firstChild.id;
+ alert(elementId);
+ 
+ if(type=="r")
+ {
+ rmove(divId);
+ }
+
+ if(type=="k")
+ kmove(divId);
  
  }
+function rmove(divId)
+{
+
+alert("in rook move function");
+alert(divId);
+
+
+}
+ 
+ function kmove(divId)
+{
+alert("in Knight move function");
+}
+
+ 
+ 
+ 
+ 
  
  
