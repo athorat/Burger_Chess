d0992ceb34ed196b6d7238fd9715b70b2fa3b25b
AbhijeetKhadake <akhadake@tavisca.com>
2014-09-01 15:04:29 +0530

    Some changes in Html and move_element  Js file

--- chess-script/move_element.js
+++ chess-script/move_element.js
@@ -1,8 +1,12 @@
+var mo = new Array();
+var l =0;
 function getId(element)
 {
+alert("in getId");
+alert(divId);
 
-			var divId=element.id;
-			alert(divId);
+			var div2Id=element.id;
+			alert(div2Id);
 			var image= document.getElementById(divId).innerHTML;
 			 alert(image);
 			// var child = divId.firstChild;
@@ -10,6 +14,19 @@ function getId(element)
 			
 			//var destId = element.id;
 			
+			 
+            document.getElementById(div2Id).innerHTML = image;
+			 mo[l] =divId+'-'+div2Id;
+			 alert(mo[l]);
+			divId=div2Id;
+			l++;
 			
-            document.getElementById('55').innerHTML = image;
+			var clear= document.getElementsByTagName("div");
+ for(z=0;z<clear.length;z++)
+ {
+ clear[z].setAttribute("style","border-color:black;");
+ }
+		document.getElementById(div2Id).setAttribute("onclick","divClick(this)");	
+			
 }
+
